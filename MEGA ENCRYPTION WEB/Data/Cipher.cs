88d91472ef7082f9b627f15c7620b82d46d19eb9
The-Vigilante <anas@vigilante@gmail.com>
2023-08-27 18:27:25 +0100

    Add project files.

---
+++ MEGA ENCRYPTION WEB/Data/Cipher.cs
@@ -0,0 +1,133 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using Microsoft.AspNetCore.Http;
+
+namespace MEGA_ENCRYPTION_WEB.Data
+{
+    public class Cipher
+    {/////////////////////////////////////Encryption///////////////////////////////////////////
+
+        private static byte[] _abc;
+        private static byte[,] _table;
+
+
+
+        public static async Task<byte[]> EncryptFileAsync(byte[] data, string psw)
+        {
+            // Initialize abc and table
+            _abc = new byte[256];
+            for (int i = 0; i < 256; i++)
+                _abc[i] = Convert.ToByte(i);
+
+            _table = new byte[256, 256];
+            for (int i = 0; i < 256; i++)
+                for (int j = 0; j < 256; j++)
+                    _table[i, j] = _abc[(i + j) % 256];
+
+            try
+            {
+                var pswTmp = Encoding.ASCII.GetBytes(psw);
+                var keys = new byte[data.Length];
+                var result = new byte[data.Length];
+                for (int i = 0; i < data.Length; i++)
+                    keys[i] = pswTmp[i % pswTmp.Length];
+
+                // Encrypt asynchronously
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        var value = data[i];
+                        var key = keys[i];
+                        int valueIndex = -1, keyIndex = -1;
+                        for (int j = 0; j < 256; j++)
+                            if (_abc[j] == value)
+                            {
+                                valueIndex = j;
+                                break;
+                            }
+                        for (int j = 0; j < 256; j++)
+                            if (_abc[j] == key)
+                            {
+                                keyIndex = j;
+                                break;
+                            }
+                        result[i] = _table[keyIndex, valueIndex];
+                    }
+                });
+
+                return result;
+            }
+            catch (Exception)
+            {
+                // Handle exceptions here
+                return null;
+            }
+        }
+
+
+
+
+
+
+
+        public static async Task<byte[]> DecryptBytesAsync(byte[] encryptedData, string psw)
+        {
+            // Initialize abc and table
+            _abc = new byte[256];
+            for (int i = 0; i < 256; i++)
+                _abc[i] = Convert.ToByte(i);
+
+            _table = new byte[256, 256];
+            for (int i = 0; i < 256; i++)
+                for (int j = 0; j < 256; j++)
+                    _table[i, j] = _abc[(i + j) % 256];
+
+            try
+            {
+                var pswTmp = Encoding.ASCII.GetBytes(psw);
+                var keys = new byte[encryptedData.Length];
+                var result = new byte[encryptedData.Length];
+                for (int i = 0; i < encryptedData.Length; i++)
+                    keys[i] = pswTmp[i % pswTmp.Length];
+
+                // Decrypt asynchronously
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < encryptedData.Length; i++)
+                    {
+                        var value = encryptedData[i];
+                        var key = keys[i];
+                        int valueIndex = -1, keyIndex = -1;
+                        for (int j = 0; j < 256; j++)
+                            if (_abc[j] == key)
+                            {
+                                keyIndex = j;
+                                break;
+                            }
+                        for (int j = 0; j < 256; j++)
+                            if (_table[keyIndex, j] == value)
+                            {
+                                valueIndex = j;
+                                break;
+                            }
+                        result[i] = _abc[valueIndex];
+                    }
+                });
+
+                return result;
+            }
+            catch (Exception)
+            {
+                // Handle exceptions here
+                return null;
+            }
+        }
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+    }
+}
---
+++ MEGA ENCRYPTION WEB/Pages/Index.cshtml.cs
@@ -0,0 +1,123 @@
+﻿using MEGA_ENCRYPTION_WEB.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MEGA_ENCRYPTION_WEB.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IWebHostEnvironment _env;
+
+        public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Password is required!")]
+        public string Password { get; set; }
+
+        public bool isFileEmpty;
+
+
+
+        public void OnGet()
+        {
+
+        }
+
+
+        public async Task<IActionResult> OnPostEncrypt()
+        {
+            try
+            {
+                if (HttpContext.Request.Form.Files.Count > 0)
+                {
+                    var file = HttpContext.Request.Form.Files[0];
+                    if (file != null && file.Length > 0)
+                    {
+                        using var memoryStream = new MemoryStream();
+                        await file.CopyToAsync(memoryStream);
+                        memoryStream.Position = 0;
+
+                        var data = memoryStream.ToArray();
+                        var encryptedData = await Cipher.EncryptFileAsync(data, Password);
+
+                        if (encryptedData != null)
+                        {
+                            var newFileName = $"{file.FileName}";
+
+                            var contentType = "application/octet-stream"; // Set the appropriate content type
+                            return File(encryptedData, contentType, newFileName);
+                        }
+                    }
+                }
+                else
+                {
+                    isFileEmpty = true;
+                    return Page();
+                }
+            }
+            catch (Exception)
+            {
+                // Handle exceptions here
+                return NotFound(); // or any other suitable response
+            }
+
+            return NotFound(); // No file was uploaded
+        }
+
+
+
+
+
+
+        public async Task<IActionResult> OnPostDecrypt()
+        {
+            try
+            {
+                if (HttpContext.Request.Form.Files.Count > 0)
+                {
+                    var file = HttpContext.Request.Form.Files[0];
+                    if (file != null && file.Length > 0)
+                    {
+                        using var memoryStream = new MemoryStream();
+                        await file.CopyToAsync(memoryStream);
+                        memoryStream.Position = 0;
+
+                        var encryptedData = memoryStream.ToArray();
+                        var decryptedData = await Cipher.DecryptBytesAsync(encryptedData, Password);
+
+                        if (decryptedData != null)
+                        {
+                            var newFileName = $"{file.FileName}";
+
+                            var contentType = "application/octet-stream"; // Set the appropriate content type
+                            return File(decryptedData, contentType, newFileName);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Handle exceptions here
+                return NotFound(); // or any other suitable response
+            }
+
+            return NotFound(); // No file was uploaded
+        }
+
+
+
+    }
+}
